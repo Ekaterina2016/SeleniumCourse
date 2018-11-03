36e9fb43903ec553b6e79f816bc4ae89f44e0cce
ekaterina2016 <ekaterina2016@skbkontur.ru>
2018-11-03 23:22:18 +0500

    Created a new project and added one simple test

---
+++ SeleniumCourse/SimpleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SKB Kontur")]
+[assembly: AssemblyProduct("SimpleTest")]
+[assembly: AssemblyCopyright("Copyright © SKB Kontur 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b234b9cb-a2cf-4670-971b-b8f660d8f0d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumCourse/SimpleTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SimpleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver chrome;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl("http://www.google.com/");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            chrome.Quit();
+        }
+    }
+}

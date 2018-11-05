68038853ece90fbf00ffb9b0e86023ad54dc050b
ekaterina2016 <ekaterina2016@list.ru>
2018-11-05 17:17:05 +0500

    added login test

--- SeleniumCourse/SimpleTest/UnitTest1.cs
+++ SeleniumCourse/SimpleTest/UnitTest1.cs
@@ -8,12 +8,26 @@ namespace SimpleTest
     public class UnitTest1
     {
         private IWebDriver chrome;
+        private string serverName = "http://localhost:8080/litecart/en/";
 
         [TestMethod]
         public void TestMethod1()
         {
             chrome = new ChromeDriver();
             chrome.Navigate().GoToUrl("http://www.google.com/");
+        }
+
+        [TestMethod]
+        public void LoginTest()
+        {
+            chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl(serverName);
+            var email = chrome.FindElement(By.Name("email"));
+            email.SendKeys("admin");
+            var password = chrome.FindElement(By.Name("password"));
+            password.SendKeys("admin");
+            var loginButton = chrome.FindElement(By.Name("login"));
+            loginButton.Click();
         }
 
         [TestCleanup]
